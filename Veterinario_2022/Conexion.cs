80d41945161b761198f6a3254a772283cc53bb61
Joel García <joel.garcia.wk@gmail.com>
2022-04-08 15:45:01 +0200

    Añadido paquete MySql y conexion.

--- Veterinario_2022/Conexion.cs
+++ Veterinario_2022/Conexion.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace Veterinario_2022
 {
     class Conexion
     {
-
+        public MySqlConnection conexion;
+        public Conexion()
+        {
+            conexion = new MySqlConnection("Server = 127.0.0.1; DataBase = ; Uid = root; Pwd =; Port = 3306;");
+        }
     }
 }
