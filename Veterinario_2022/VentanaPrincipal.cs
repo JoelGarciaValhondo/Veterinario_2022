b99828df784fd3594d348a9b34a321a97e80b067
Joel García <joel.garcia.wk@gmail.com>
2022-04-13 16:45:33 +0200

    Comentarios varios y algo de diseño de añadir usuarios.

--- Veterinario_2022/VentanaPrincipal.cs
+++ Veterinario_2022/VentanaPrincipal.cs
@@ -29,9 +29,15 @@ namespace Veterinario_2022
 
         private void insertaUsuario_Click(object sender, EventArgs e)
         {
+            //HASH DE LA PASS AL INTRODUCIRLA POR PRIMERA VEZ EN LA BBDD.
             String textoPassword = textBoxPass.Text;
             String myHash = BCrypt.Net.BCrypt.HashPassword(textoPassword, BCrypt.Net.BCrypt.GenerateSalt());
             miConexion.insertaUsuario(textBoxDNI.Text, textBoxNombre.Text, textBoxApellido.Text, myHash, textBoxEmail.Text);
+        }
+
+        private void VentanaPrincipal_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
